5c5659d1d35a380738bc90bd4be3f7b2aa0d027e
Ahmet Turk <ahmet@miops.com>
2023-02-16 00:36:27 +0300

    Added Bottom App Bar

--- app/src/main/java/com/ahmetturk/socially/MainActivity.kt
+++ app/src/main/java/com/ahmetturk/socially/MainActivity.kt
@@ -1,13 +1,49 @@
 package com.ahmetturk.socially
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.navigation.fragment.NavHostFragment
+import com.ahmetturk.socially.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment -> {
+                    binding.bottomAppBar.visibility = View.GONE
+                    binding.addButton.visibility = View.GONE
+                }
+                R.id.homepageFragment -> {
+                    binding.bottomAppBar.visibility = View.VISIBLE
+                    binding.addButton.visibility = View.VISIBLE
+                    updateMenuColors(binding.homeButton)
+                }
+            }
+        }
+    }
+
+    private fun updateMenuColors(selected: MaterialButton) {
+        val menuList = binding.menuConstraintLayout.children.toMutableList()
+        menuList.remove(selected)
+
+        selected.setIconTintResource(R.color.selected_menu)
+
+        menuList.forEach {
+            (it as MaterialButton).setIconTintResource(R.color.black)
+        }
     }
 }
